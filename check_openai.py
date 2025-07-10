f48afa6481fdaeecb3140c0cff676491750cf69e
alelsandr <sanya10081999@gmail.com>
2025-07-10 17:17:46 +0700

    fix: check_openai.py

--- check_openai.py
+++ check_openai.py
@@ -3,12 +3,10 @@ import httpx
 
 
 def check_openai():
-
     client = httpx.Client()
-
     response = client.post('http://0.0.0.0:8000/api/complaints/', json={
         'text': 'You are from technical developer!!!'
-    })
+    }, timeout=10)
 
     print(response.status_code)
     print(response.text)
